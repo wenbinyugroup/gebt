8edd4625e88a6496b26c2258ec7fb3154e411315
Su Tian <sutian@analyswift.com>
2024-11-20 21:28:01 +0800

    update doc

--- README.md
+++ README.md
@@ -12,13 +12,11 @@ User manual and other supporting document are in `doc`.
 
 ## Build from source
 
-Need to request ma28 library first and make some changes according to Tutorial_GEBT_Installation.pdf.
-
-To compile the code, you need to use the makefile in the bin directory.
 
 
 ### Linux
 
+To compile the code, you need to use the makefile in the bin directory.
 
 
 ### Windows
--- doc/source/conf.py
+++ doc/source/conf.py
@@ -24,6 +24,20 @@ extensions = [
 templates_path = ['_templates']
 exclude_patterns = []
 
+html_theme_options = {
+    'logo': {
+        'text': 'GEBT',
+    },
+    'show_nav_level': 2,
+    # "path_to_docs": "doc/source",
+    # 'use_edit_page_button': True,
+    # "use_repository_button": True,
+    # "use_issues_button": True,
+    # 'collapse_navigation': True,
+    'navigation_depth': 4,
+    "announcement": "Documentation is under construction.",
+}
+
 
 
 # -- Options for HTML output -------------------------------------------------
